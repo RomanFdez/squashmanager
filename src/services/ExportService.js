b173001b8461cfd914c1a470eb842aeb04dcd317
RomanFdez <a.roman.fernandez@gmail.com>
2026-01-16 20:39:00 +0100

    Fix CSV encoding for special characters (BOM)

--- src/services/ExportService.js
+++ src/services/ExportService.js
@@ -26,7 +26,7 @@ export const exportToCSV = (data, filename) => {
         }).join(','))
     ].join('\n');
 
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const blob = new Blob(["\uFEFF" + csvContent], { type: 'text/csv;charset=utf-8;' });
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.setAttribute('href', url);
